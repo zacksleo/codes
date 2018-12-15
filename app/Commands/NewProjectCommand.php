dfa85fad0af4f49710733f6292e5a2320e7c3a4f
zacksleo <zacksleo@gmail.com>
2018-12-15 12:45:35 +0800

    refact(app): new project command

--- app/Commands/NewProjectCommand.php
+++ app/Commands/NewProjectCommand.php
@@ -19,7 +19,7 @@ class NewProjectCommand extends GeneratorCommand
      *
      * @var string
      */
-    protected $description = '创建新的项目';
+    protected $description = 'New Project';
 
     public function getStub()
     {
@@ -33,15 +33,15 @@ class NewProjectCommand extends GeneratorCommand
      */
     public function handle()
     {
-        $id = $this->ask('请输入项目代码：');
+        $id = $this->ask('Code');
         $path = getcwd() . "/$id/";
         if (file_exists($path)) {
-            $this->error('项目已存在');
+            $this->error('Project Exists');
             return false;
         }
-        $name = $this->ask('请输入项目名称：');
-        $template = $this->choice('请选择要使用的模板', ['yii2', 'taro', 'react-navive'], 'yii2');
-        $email = $this->ask('请输入管理员邮箱：');
+        $name = $this->ask('Name');
+        $template = $this->choice('Template', ['yii2'], 'yii2');
+        $email = $this->ask('Admin Email');
         $envs = [
             'id' => $id,
             'name' => $name,
@@ -68,27 +68,10 @@ class NewProjectCommand extends GeneratorCommand
         }
         switch ($template) {
             case 'yii2':
-                $this->buildYii($name, $envs, $path);
+                $this->buildEnvs($envs, $path);
                 break;
         }
         $this->info('created successfully.');
-    }
-
-    /**
-     * Build the class with the given name.
-     *
-     * @param  string  $name
-     * @param string path
-     */
-    protected function buildYii($name, $envs, $path)
-    {
-        $this->comment('setings configs...');
-        $modules = ['api', 'frontend', 'backend'];
-        foreach ($modules as $module) {
-            $filePath = $path . '/' . $module . '/config/main-local.php';
-            $this->files->put($filePath, $this->buildMainLocal($name, $filePath));
-        }
-        $this->buildEnvs($envs, $path);
     }
 
     protected function buildEnvs($envs, $path)
@@ -99,23 +82,12 @@ class NewProjectCommand extends GeneratorCommand
             $filePath = $path . '/' . $module . '.env';
             $stub = $this->files->get($filePath);
             $stub = str_replace(
-                ['DummyAppName', 'DummyAdminEmail', 'DummyId', 'DummyDbPassword'],
-                [$envs['name'], $envs['email'], $envs['id'], str_random(12)],
+                ['DummyAppName', 'DummyAdminEmail', 'DummyId', 'DummyDbPassword', 'DummyValidationKey'],
+                [$envs['name'], $envs['email'], $envs['id'], str_random(12), strtoupper(md5($envs['name']))],
                 $stub
             );
             $this->files->put($filePath, $stub);
         }
-    }
-
-    protected function buildMainLocal($name, $path)
-    {
-        $stub = $this->files->get($path);
-        $stub = str_replace(
-            ['DummyValidationKey', ],
-            [$this->getValidationKey($name)],
-            $stub
-        );
-        return $stub;
     }
 
     protected function getValidationKey($name)
